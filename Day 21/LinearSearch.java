7822450f78caf67971e05147f8ba7880ecf01adc
Ranjzth <ranjithkeestu@gmail.com>
2026-01-21 10:02:02 +0530

    Search

---
+++ Day 21/LinearSearch.java
@@ -0,0 +1,28 @@
+class LinearSearch
+{
+    
+    public int linearSearch(int[] arr, int searchvalue)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == searchvalue)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+	
+	
+	
+	public static void main(String[] args)
+    {
+        int[] arr = {10, 20, 30, 40, 50};
+        int serachValue = 30;
+        
+        LinearSearch bk = new LinearSearch();
+        int index = bk.linearSearch(arr, serachValue);
+        System.out.println(index);
+    }
+	
+}
