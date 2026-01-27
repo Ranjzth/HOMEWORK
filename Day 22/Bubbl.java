757c92c0b5e973530da92955262c60956893ba01
Ranjzth <ranjithkeestu@gmail.com>
2026-01-27 11:15:58 +0530

    MenuArray

--- Day 22/Bubbl.java
+++ Day 22/Bubbl.java
@@ -1,11 +1,11 @@
-class Bubbl 
+class Bubble
 {
 
     void bubbleSort(int arr[]) 
 	{
 
-        for (int j = 0; j < arr.length - 1; j++) {
-            for (int i = 0; i < arr.length - j - 1; i++) {
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr.length-1; i++) {
                 if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
